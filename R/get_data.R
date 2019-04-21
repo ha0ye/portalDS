81de6f1315e39df9c013d61febf9f1a8d6299ca8
Hao Ye <lhopitalified@gmail.com>
2019-04-21 15:10:08 -0400

    fix portal data function

--- R/get_data.R
+++ R/get_data.R
@@ -5,12 +5,13 @@
 #' @param filter_q the numerical quantile by which to filter species. Only
 #'   species that are present at least `filter_q` fraction of the time are
 #'   included; default (NULL) keeps all species.
-#' @inheritParams portalr::get_rodent_data
+#' @inheritParams portalr::summarize_rodent_data
 #'
 #' @return a data.frame with columns for `censusdate`, and each species
 #'
 #' @export
-make_portal_block <- function(filter_q = NULL, output = "abundance",
+make_portal_block <- function(path = portalr::get_default_data_path(), 
+                              filter_q = NULL, output = "abundance",
                               plots = c(2, 4, 8, 11, 12, 14, 17, 22), ...)
 {
     # options here are:
@@ -18,21 +19,22 @@ make_portal_block <- function(filter_q = NULL, output = "abundance",
     #   level = "plot" (allow us to pull out abundances on the plots we want)
     #   effort = TRUE (so we can check effort)
     #   na_drop = TRUE (ignore periods where sampling did not occur)
-    raw_rodent_data <- portalr::get_rodent_data(time = "all",
-                                                plots = plots,
-                                                effort = TRUE,
-                                                na_drop = TRUE,
-                                                output = output,
-                                                ...)
-
+    raw_rodent_data <- portalr::summarize_rodent_data(path = path, 
+                                                      time = "all",
+                                                      plots = plots,
+                                                      effort = TRUE,
+                                                      na_drop = TRUE,
+                                                      output = output,
+                                                      ...)
+    
     # summarize by each newmmonnumber, and for only the control plots we want
     block <- raw_rodent_data %>%
         dplyr::filter(censusdate < "2015-04-18") %>%
         dplyr::select(-period)
-
+    
     # check that effort is equal across samples
     stopifnot(length(unique(block$ntraps)) == 1)
-
+    
     if (!is.null(filter_q))
     {
         species_list <- block %>%
@@ -41,11 +43,11 @@ make_portal_block <- function(filter_q = NULL, output = "abundance",
             dplyr::summarize(quantile_q = quantile(abundance, 1 - filter_q)) %>%
             dplyr::filter(quantile_q > 0) %>%
             dplyr::pull(species)
-
+        
         block <- block %>%
             dplyr::select(c("newmoonnumber", "censusdate", "ntraps", "nplots", species_list))
     }
-
+    
     # add in NAs for unsampled newmoonnumbers and interpolate
     block <- block %>%
         tidyr::complete(newmoonnumber = tidyr::full_seq(newmoonnumber, 1), fill = list(NA)) %>%
@@ -53,7 +55,7 @@ make_portal_block <- function(filter_q = NULL, output = "abundance",
         dplyr::mutate_at(dplyr::vars(-newmoonnumber, -ntraps), as.numeric) %>%
         dplyr::mutate(censusdate = as.Date(as.numeric(censusdate), origin = "1970-01-01")) %>%
         dplyr::select(-newmoonnumber, -ntraps, -nplots)
-
+    
     return(block)
 }
 
@@ -79,19 +81,19 @@ make_surrogate_annual_spline <- function(day_of_year, ts, num_surr = 100)
     idx <- which(is.finite(day_of_year) & is.finite(y))
     day_of_year <- day_of_year[idx]
     ts <- ts[idx]
-
+    
     xx <- c(day_of_year - 365, day_of_year, day_of_year + 365)
     yy <- c(ts, ts, ts)
     seasonal_F <- smooth.spline(xx, yy)
     seasonal_cyc <- predict(seasonal_F, day_of_year)$y
     seasonal_resid <- ts - seasonal_cyc
-
+    
     vals <- matrix(unlist(
         lapply(seq(num_surr), function(i) {
             seasonal_cyc + sample(seasonal_resid, n)
         })
     ), ncol = num_surr)
-
+    
     out <- matrix(NA, nrow = n, ncol = num_surr)
     out[idx, ] <- vals
 }
--- tests/testthat/test_02_get_data.R
+++ tests/testthat/test_02_get_data.R
@@ -1,11 +1,14 @@
 context("Check get_data produces identical results")
 
+portal_data_path <- tempdir()
+
 test_that("portalr downloads data ok", {
-    expect_error(portalr::download_observations(version = "1.66.0"), NA)
+    expect_error(portalr::download_observations(path = portal_data_path, 
+                                                version = "1.66.0"), NA)
 })
 
 test_that("make_portal_block works with default values", {
-    expect_error(portal_block <- make_portal_block(), NA)
+    expect_error(portal_block <- make_portal_block(path = portal_data_path), NA)
     expect_true(exists("portal_block"))
     expect_equal(dim(portal_block), c(440, 22))
     expect_true("censusdate" %in% names(portal_block))
@@ -15,7 +18,8 @@ test_that("make_portal_block works with default values", {
 })
 
 test_that("make_portal_block works with filtering by 50% present", {
-    expect_error(portal_block_50 <- make_portal_block(filter_q = 0.50), NA)
+    expect_error(portal_block_50 <- make_portal_block(path = portal_data_path, 
+                                                      filter_q = 0.50), NA)
     expect_true(exists("portal_block_50"))
     expect_equal(dim(portal_block_50), c(440, 8))
     expect_true("censusdate" %in% names(portal_block_50))
