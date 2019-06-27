fe8670ce4534853bbdaf6c77f70f5048b1b00f5c
Hao Ye <lhopitalified@gmail.com>
2019-06-27 16:03:55 -0400

    further simplification of drake plans

--- R/calculations-ccm.R
+++ R/calculations-ccm.R
@@ -47,8 +47,8 @@ compute_ccm <- function(simplex_results,
         # generate surrogates and compute CCM
         surr_ts <- simplex_results[[from_idx, "surrogate_data"]]
         
-        ccm_surr <- purrr::map_dfr(seq(NCOL(surr_ts)),
-                                   ~ccm_func(cbind(surr_ts[, .], pred_ts), E)) %>%
+        ccm_surr <- purrr::map_dfr(data.frame(surr_ts),
+                                   ~ccm_func(cbind(., pred_ts), E)) %>%
             dplyr::mutate(data_type = "surrogate")
         
         # combine outputs
@@ -79,42 +79,42 @@ build_ccm_plan <- function(lib_sizes = seq(10, 100, by = 10),
                            silent = TRUE)
 {
     drake::drake_plan(
-        ccm_func = function(df, E) {
-            rEDM::ccm(df, E = E, lib_sizes = !!lib_sizes,
-                      random_libs = !!random_libs, num_samples = !!num_samples,
-                      replace = !!replace,
-                      lib_column = 1, target_column = 2,
-                      RNGseed = !!RNGseed, silent = !!silent) %>%
-                rEDM::ccm_means(na.rm = TRUE) %>%
-                dplyr::select(lib_size, num_pred, rho, mae, rmse)},
+        ccm_func = function(from_idx, to_idx, E, from_var, to_var) {
+            compute_ccm <- function(df, E) {
+                rEDM::ccm(df, E = E, lib_sizes = !!lib_sizes,
+                          random_libs = !!random_libs, num_samples = !!num_samples,
+                          replace = !!replace,
+                          lib_column = 1, target_column = 2,
+                          RNGseed = !!RNGseed, silent = !!silent) %>%
+                    rEDM::ccm_means(na.rm = TRUE) %>%
+                    dplyr::select(lib_size, num_pred, rho, mae, rmse)}
+            # pull out variables from the original block
+            lib_ts <- simplex_results[[from_idx, "data"]]$abundance
+            pred_ts <- simplex_results[[to_idx, "data"]]$abundance
+            
+            # compute CCM for actual connection
+            ccm_actual <- compute_ccm(cbind(lib_ts, pred_ts), E) %>%
+                dplyr::mutate(data_type = "actual")
+            # generate surrogates and compute CCM
+            surr_ts <- simplex_results[[from_idx, "surrogate_data"]]
+            
+            ccm_surr <- purrr::map_dfr(data.frame(surr_ts),
+                                       ~ccm_func(cbind(., pred_ts), E)) %>%
+                dplyr::mutate(data_type = "surrogate")
+            
+            # combine outputs
+            ccm_out <- dplyr::bind_rows(ccm_actual, ccm_surr) %>%
+                dplyr::mutate(lib_column = from_var,
+                              target_column = to_var)
+            ccm_out$E <- E
+            return(ccm_out)
+        }, 
         ccm_params = expand.grid(from_idx = seq(NROW(simplex_results)),
                                  to_idx = seq(NROW(simplex_results))) %>%
             dplyr::mutate(E = simplex_results$best_E[from_idx],
                           from_var = simplex_results$species[from_idx],
                           to_var = simplex_results$species[to_idx]),
-        ccm_results = furrr::future_pmap(ccm_params,
-                                         function(from_idx, to_idx, E, from_var, to_var) {
-                                             # pull out variables from the original block
-                                             lib_ts <- simplex_results[[from_idx, "data"]]$abundance
-                                             pred_ts <- simplex_results[[to_idx, "data"]]$abundance
-                                             
-                                             # compute CCM for actual connection
-                                             ccm_actual <- ccm_func(cbind(lib_ts, pred_ts), E) %>%
-                                                 dplyr::mutate(data_type = "actual")
-                                             # generate surrogates and compute CCM
-                                             surr_ts <- simplex_results[[from_idx, "surrogate_data"]]
-                                             
-                                             ccm_surr <- purrr::map_dfr(seq(NCOL(surr_ts)),
-                                                                        ~ccm_func(cbind(surr_ts[, .], pred_ts), E)) %>%
-                                                 dplyr::mutate(data_type = "surrogate")
-                                             
-                                             # combine outputs
-                                             ccm_out <- dplyr::bind_rows(ccm_actual, ccm_surr) %>%
-                                                 dplyr::mutate(lib_column = from_var,
-                                                               target_column = to_var)
-                                             ccm_out$E <- E
-                                             return(ccm_out)
-                                         }) %>%
+        ccm_results = furrr::future_pmap(ccm_params, ccm_func) %>%
             dplyr::bind_rows() %>%
             dplyr::select(lib_column, target_column, data_type, dplyr::everything()) %>%
             dplyr::mutate_at(c("lib_column", "target_column", "data_type"), as.factor)
